2f5bcbc62bfb7c4bc1cb34fd1ba60822be69630e
Henrique Agustini <henrique80@gmail.com>
2022-04-08 00:59:42 -0300

    :package: updates

---
+++ app.js
@@ -0,0 +1,82 @@
+const tiles = document.querySelector('.title-container')
+const backspaceAndEnterRow = document.querySelector('#backspaceAndEnterRow')
+const keyboardFirstRow = document.querySelector('#keyboardFirstRow')
+const keyboardSecondRow = document.querySelector('#keyboardSecondRow')
+const keyboardThirdRow = document.querySelector('#keyboardThirdRow')
+
+const keysFirstRow = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+const keysSecondRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+const keysThirdRow = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+const rows = 6
+const columns = 5
+let currentRow = 0
+let currentColumn = 0
+const letreco = 'vasco'
+const guesses = []
+
+for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+  guesses[rowIndex] = new Array(columns)
+  const tileRow = document.createElement('div')
+  tileRow.setAttribute('id', 'row' + rowIndex)
+  tileRow.setAttribute('class', 'tile-row')
+  for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
+    const tileColumn = document.createElement('div')
+    tileColumn.setAttribute('id', 'row' + rowIndex + 'column' + columnIndex)
+    tileColumn.setAttribute('class', rowIndex === 0 ? 'tile-column typing' : 'tile-column disabled');
+    tileRow.append(tileColumn)
+    guesses[rowIndex][columnIndex] = ''
+  }
+  tiles.append(tileRow)
+}
+
+const checkGuess = () => {
+  const guess = guesses[currentRow].join("");
+  if (guess.length !== columns) {
+    return
+  }
+
+  
+
+}
+
+const handleKeyboardOnClick = key => {
+  if (currentColumn === columns) {
+    return
+  }
+
+  const currentTile = document.querySelector(
+    '#row' + currentRow + 'column' + currentColumn
+  )
+  currentTile.textContent = key
+  guesses[currentRow][currentColumn] = key
+  currentColumn++
+}
+
+const createKeyboardRow = (keys, keyboardRow) => {
+  keys.forEach(key => {
+    var buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', () => handleKeyboardOnClick(key))
+    keyboardRow.append(buttonElement)
+  })
+}
+
+createKeyboardRow(keysFirstRow, keyboardFirstRow)
+createKeyboardRow(keysSecondRow, keyboardSecondRow)
+createKeyboardRow(keysThirdRow, keyboardThirdRow)
+
+const handleBackspace = () => {
+  console.log('apaga ')
+}
+const backspaceButton = document.createElement('button');
+backspaceButton.addEventListener('click', handleBackspace);
+backspaceButton.textContent = '<';
+backspaceAndEnterRow.append(backspaceButton);
+
+
+const enterButton = document.createElement('button');
+enterButton.addEventListener('click', checkGuess);
+enterButton.textContent = 'ENTER';
+backspaceAndEnterRow.append(enterButton);
